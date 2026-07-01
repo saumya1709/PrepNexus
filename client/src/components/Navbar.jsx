55d9e7c9db2f32b8da3db795e5bbff0d296fc0d2
saumya1709 <saumyamaheshwari17@gmail.com>
2026-07-01 19:21:40 +0530

    Change Navbar title from InterviewIQ.AI to PrepNexus

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
           <div className="bg-black text-white p-2 rounded-lg">
             <BsRobot size={18}/>
           </div>
-          <h1 className="font-semibold md:block text-lg">InterviewIQ.AI</h1>
+          <h1 className="font-semibold md:block text-lg">PrepNexus</h1>
         </div>
 
         <div className="flex items-center gap-6 relative">
