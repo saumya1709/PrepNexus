b943b8374ddab6580245b707e6b740c26da2eb97
saumya1709 <saumyamaheshwari17@gmail.com>
2026-07-01 19:14:30 +0530

    Rename HR image file to match casing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ import { useState } from "react";
 import AuthModel from "../components/AuthModel";
 import { useNavigate } from "react-router-dom";
 import evalImg from "../assets/ai-ans.png";
-import hrImg from "../assets/hr.png";
+import hrImg from "../assets/HR.png";
 import techImg from "../assets/tech.png";
 import confidenceImg from "../assets/confi.png";
 import creditImg from "../assets/credit.png";
